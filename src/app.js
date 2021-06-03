00438f304613bf6426be115a533a1098d65b15e6
Samuel Melloul <Samuel@Samuels-MacBook-Air.local>
2021-06-03 09:58:01 +0300

    New setup for Heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 const app = express()
-const port = process.env || 3000
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
